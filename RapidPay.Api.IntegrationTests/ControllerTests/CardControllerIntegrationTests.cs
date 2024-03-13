736dafd4a2bc22465f931a9a5920525023cf9cc4
Gaurav Rawat <gauravrawat@abhiks-MacBook-Pro.local>
2024-03-13 16:18:38 +0530

    initial

---
+++ RapidPay.Api.IntegrationTests/ControllerTests/CardControllerIntegrationTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RapidPay.Models;
+using RapidPay.Services;
+using Serilog;
+using RapidPayAPI.Controllers;
+using Xunit;
+using Microsoft.Extensions.Logging;
+
+namespace RapidPay.Api.IntegrationTests.ControllerTests
+{
+    public class CardControllerIntegrationTests : IDisposable
+    {
+        private CardController _controller;
+        private ApplicationDbContext _context;
+        private Card _testCard;
+        private Logger<CardController> _logger;
+        
+
+        public CardControllerIntegrationTests()
+        {
+            // Configure in-memory database for testing
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            _context = new ApplicationDbContext(options);
+
+            _testCard = new Card(300.0);
+
+            
+            _testCard.CardNumber = "123456789123456";
+
+            _context.Cards.Add(_testCard);
+            _context.SaveChanges();
+
+
+            // Mock UniversalFeesExchange
+            var feesExchange = new UniversalFeesExchange();
+
+            _controller = new CardController(_context, feesExchange, _logger);
+        }
+
+        [Fact]
+        public async Task CreateCard_ValidRequest_ReturnsOk()
+        {
+            // Arrange
+            var cardRequest = new Card(200.0);
+
+            var result = await _controller.CreateCard(cardRequest);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            var card = okResult.Value as Card;
+            Assert.NotNull(card);
+            Assert.Equal(cardRequest.Balance, card.Balance);
+        }
+
+        [Fact]
+        public async Task Pay_InsufficientBalance_ReturnsBadRequest()
+        {   
+            var amount = 300.0;
+             
+                // Act
+                var result = await _controller.Pay(_testCard.CardNumber, amount);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task GetBalance_ExistingCard_ReturnsOkWithBalance()
+        {
+  
+
+            // Act
+            var result = await _controller.GetBalance(_testCard.CardNumber);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult.Value);
+            Assert.Equal($"Card {_testCard.CardNumber} balance: 300", okResult.Value.ToString());
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
+
+
---
+++ RapidPay/Migrations/20240312160012_NewInitialisation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RapidPay.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewInitialisation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
